e4f247eebdd19f86c5ca651dd2eb66b899b79f40
Srinivasan G <46883734+SrinivasanGajendran@users.noreply.github.com>
2023-04-11 11:42:23 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -6,22 +6,28 @@ import pandas as pd
 import tempfile
 import mysql.connector
 
+#-------------------------------------------Page_Config------------------------------------
 
 st.set_page_config(page_title='TEXT_EXTRACTION_USING_OCR', page_icon=":tada", layout='wide')
 st.title('Extraction Of data Using Python And EasyOCR')
+
+#----------------------------------------Creating a select Menu-----------------------------
+
 SELECT = option_menu(
     menu_title=None,
-    options=['Home', 'Process','Database'],
+    options=['Home', 'Process', 'Database'],
     icons=['house', 'bar-chart'],
     default_index=2,
     orientation='horizontal',
     styles={
         'container': {'padding': '0!important', 'background-color': 'white', 'size': 'cover'},
         'icon': {'color': 'white', 'font-size': '20px'},
-        'nav-link': {'font-size': '20px', 'text-align': 'center', 'margin': '-2px', '--hover-color': '#0b0214'},
-        'nav-link-selected': {'background-color': '#0b0214'}
+        'nav-link': {'font-size': '20px', 'text-align': 'center', 'margin': '-2px', '--hover-color': '#808080'},
+        'nav-link-selected': {'background-color': '#808080'}
     }
 )
+
+#----------------------Home--------------------------------------
 
 if SELECT == 'Home':
     st.write('Extraction of data using Python and EasyOCR refers to the process of using the EasyOCR library in Python to extract text data from images, PDFs, or other types of documents. EasyOCR is a Python library that provides a simple way to perform Optical Character Recognition (OCR) on images or scanned documents, using deep learning models.'
@@ -31,21 +37,21 @@ if SELECT == 'Home':
     image = Image.open('D:/python/Capstone_4/ocr.jpg')
     st.image(image, caption='EasyOCR')
 
+#------------------------Process--------------------------------
+    
 elif SELECT == 'Process':
-    uploaded_file = st.file_uploader("Choose a file",type=['png','jpg'])
+    uploaded_file = st.file_uploader('Choose a image file')
     if uploaded_file is not None:
-        # get the file name
-        file_name = uploaded_file.name
-        # create a temporary file
-        with tempfile.NamedTemporaryFile(delete=False, suffix=file_name) as temp_file:
-            # write the file contents to the temporary file
-            temp_file.write(uploaded_file.getvalue())
-            # get the file path
-            file_path = temp_file.name
-            if st.button('Upload'):
-                main.extraction(file_path)
-                st.success('Created DB')
-
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            f.write(uploaded_file.getvalue())
+            uploaded_filename = f.name
+        if st.button('Upload'):
+            main.extraction(uploaded_filename)
+            st.success('Created DB')
+        os.unlink(uploaded_filename) #-------------------------> To delete the temp file created
+        
+        
+#----------------------------Database---------------------------
 
 elif SELECT == 'Database':
                 conn = mysql.connector.connect(
@@ -55,11 +61,15 @@ elif SELECT == 'Database':
                 database="testing"
                 )
                 cursor = conn.cursor()
-                #df = pd.read_sql_query('SELECT * FROM Business_card', conn)
-                df1 = pd.read_sql_query('SELECT Name FROM Business_card', conn)
-                #st.write(df)
-                options = st.selectbox('Search By Name',(df1))
-                df2 = pd.read_sql_query(f"""SELECT * FROM Business_card WHERE Name = '{options}' """, conn)
-                st.write(df2)
-
-
+                df1 = pd.read_sql_query('SELECT * FROM Business_card', conn)
+                column_names = df1.columns.tolist()
+                options = st.selectbox('Search By Name',(column_names))
+                title = st.text_input(f'{options}')
+                df2 = pd.read_sql_query(f"""SELECT * FROM Business_card WHERE {options} = '{title}' """, conn)
+                if st.button('Apply'):
+                    st.write(df2)
+                if st.button('View DB'):
+                    df = pd.read_sql_query('SELECT * FROM Business_card', conn)
+                    st.write(df)
+                conn.commit()
+                conn.close()
