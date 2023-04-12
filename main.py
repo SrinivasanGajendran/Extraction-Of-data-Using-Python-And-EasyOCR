c1bedc5e08606d0eb022a40279fff28245bea4fa
Srinivasan G <46883734+SrinivasanGajendran@users.noreply.github.com>
2023-04-12 10:04:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ import mysql.connector
 import pandas as pd
 from PIL import Image
 import io
-
-#----------This is to view full dataframe in pycharm-------
 desired_width = 320
 pd.set_option('display.width',desired_width)
 pd.set_option('display.max_columns',20)
-#----------------------------------------------------------
 
 def extraction(file_path):
+
             global Mobile_Number, Email_id, Website, Address, Alternate_number
             details = []
             dup = []
@@ -24,31 +22,29 @@ def extraction(file_path):
             phone_regex = r'(?:(?<=\s)|(?<=^))(?:Phone:|Mobile:)?\s*[\+]?[1-9][0-9 .\-\(\)]{8,}[0-9]'
             address_regex = r'\b\d+\s+\w+\s+\w+\s*,?\s*\w*\.?\s*\w+,\s*\w+\s+\d+\b'
             pattern = r"^[A-Z][a-zA-Z]*\s[A-Z][a-zA-Z]*$"
-            
-            
-            #---------------------------------------------------------------Fetching The Name--------------------------------------------------------------------->
+            #Fetching The Name
             Name = result[0][1]
-            
-            #------------------------------------------------------------Fetching The Designation----------------------------------------------------------------->
+            #Fetching The Designation
             Designation = result[1][1]
-            
-            #-------------------------------------------------------------Phone_Number Filetred------------------------------------------------------------------->
+            count = 0
             for ele in result:
+                #Phone_Number Filetred
+
                 if re.findall(phone_regex,ele[1]):
                     ph_num = ''
                     Alternate_number = ''
                     ph_num += ele[1]
                     dup.append(ph_num)
+                    #count += 1
                     if len(dup)==1:
                         Mobile_Number = ph_num
                     elif len(dup)==2:
                         Alternate_number = ph_num
 
-            #----------------------------------------------------------------mail-Id Filtered--------------------------------------------------------------------->
+                # mail-Id Filtered
                 if '@' in ele[1]:
                     Email_id = ele[1]
 
-            #--------------------------------------------------------------------Website-------------------------------------------------------------------------->
             for el in tes:
                 if el.lower().startswith('www'):
                     Website = el
@@ -57,7 +53,7 @@ def extraction(file_path):
                         id = el+'.'+tes[ind]
                         Website = id
 
-            #----------------------------------------------------------------Address Filter---------------------------------------------------------------------->
+            #Address Filter
             for ele in res:
                 if ele[1].startswith('123 '):
                     Address = ele[1]
@@ -66,26 +62,23 @@ def extraction(file_path):
                     if address_match:
                         addresess = address_match.group(0)
                         Address  = addresess
-                        
-            #---------------------------------------------------------Appending the details in a list and convrting it into a dataframe-------------------------->
-            
+
             details.append({'Name':Name,'Designation':Designation,'Mobile_Number':Mobile_Number,'Alternate_Number':Alternate_number,'Email_id':Email_id,'Website':Website,'Address':Address})
             df = pd.DataFrame(details,columns=['Name','Designation','Mobile_Number','Alternate_Number', 'Email_id', 'Website', 'Address'])
-            
-            #---------------------------------------------------------Connecting with the DB--------------------------------------------------------------------->
+
+            with open(file_path, 'rb') as file:
+                binaryData = file.read()
             conn = mysql.connector.connect(
                 host="localhost",
                 user="srini",
                 password="password"
             )
+            # Create a cursor object
             cursor = conn.cursor()
-            #------------------------------------------------------Check if the database exists----------------------------------------------------------------->
+            # Check if the database exists
             sql = "SHOW DATABASES LIKE 'Bizcard'"
             cursor.execute(sql)
             result = cursor.fetchone()
-            
-            #---------------------------------------------------------If it exists excutes 'if' loop------------------------------------------------------------>
-            
             if result:
                 conn = mysql.connector.connect(
                     host="localhost",
@@ -93,21 +86,33 @@ def extraction(file_path):
                     password="password",
                     database="Bizcard"
                 )
-                for i,row in df.iterrows():
-                  sql = ("""INSERT INTO Business_Card (Name, Designation,Mobile_Number,Alternate_Number, Email_id, Website, Address) VALUES(%s, %s,%s, %s, %s, %s,%s)""")
-                  val = (row['Name'],row['Designation'],row['Mobile_Number'],row['Alternate_Number'],row['Email_id'],row['Website'],row['Address'])
-                  cursor = conn.cursor()
-                  cursor.execute(sql, val)
-                  conn.commit()
-            
-            #--------------------------------------------------------Else creates a DB and executes------------------------------------------------------------>
-            
+                cursor = conn.cursor()
+                sql = "SHOW TABLES LIKE 'Business_Card'"
+                cursor.execute(sql)
+                result_t = cursor.fetchone()
+                if result_t:
+                    for i,row in df.iterrows():
+                      sql = ("""INSERT INTO Business_Card (Name, Designation,Mobile_Number,Alternate_Number, Email_id, Website, Address, Image) VALUES(%s, %s, %s,%s, %s, %s, %s,%s)""")
+                      val = (row['Name'],row['Designation'],row['Mobile_Number'],row['Alternate_Number'],row['Email_id'],row['Website'],row['Address'],binaryData)
+                      cursor = conn.cursor()
+                      cursor.execute(sql, val)
+                      conn.commit()
+                else:
+                    cursor.execute("CREATE TABLE Business_Card (ID  INTEGER  AUTO_INCREMENT PRIMARY  KEY, Name TEXT, Designation TEXT, Mobile_Number VARCHAR(20),Alternate_Number VARCHAR(20), Email_id TEXT,Website TEXT, Address TEXT,Image LONGBLOB)")
+                    for i,row in df.iterrows():
+                      sql = ("""INSERT INTO Business_Card (Name, Designation,Mobile_Number,Alternate_Number, Email_id, Website, Address, Image) VALUES(%s, %s, %s,%s, %s, %s, %s,%s)""")
+                      val = (row['Name'],row['Designation'],row['Mobile_Number'],row['Alternate_Number'],row['Email_id'],row['Website'],row['Address'],binaryData)
+                      cursor = conn.cursor()
+                      cursor.execute(sql, val)
+                      conn.commit()
+
             else:
                 conn = mysql.connector.connect(
                     host="localhost",
                     user="srini",
                     password="password"
                 )
+                # Create a cursor object
                 cursor = conn.cursor()
                 sql = "CREATE DATABASE Bizcard"
                 cursor.execute(sql)
@@ -118,10 +123,10 @@ def extraction(file_path):
                     database="Bizcard"
                 )
                 cursor = conn.cursor()
-                cursor.execute("CREATE TABLE Business_Card (ID  INTEGER  AUTO_INCREMENT PRIMARY  KEY, Name TEXT, Designation TEXT, Mobile_Number VARCHAR(20),Alternate_Number VARCHAR(20), Email_id TEXT,Website TEXT, Address TEXT)")
+                cursor.execute("CREATE TABLE Business_Card (ID  INTEGER  AUTO_INCREMENT PRIMARY  KEY, Name TEXT, Designation TEXT, Mobile_Number VARCHAR(20),Alternate_Number VARCHAR(20), Email_id TEXT,Website TEXT, Address TEXT,Image LONGBLOB)")
                 for i, row in df.iterrows():
-                    sql = ("""INSERT INTO Business_Card (Name, Designation,Mobile_Number,Alternate_Number, Email_id, Website, Address) VALUES(%s, %s, %s, %s, %s, %s, %s)""")
-                    val = (row['Name'], row['Designation'], row['Mobile_Number'],row['Alternate_Number'], row['Email_id'], row['Website'], row['Address'])
+                    sql = ("""INSERT INTO Business_Card (Name, Designation,Mobile_Number,Alternate_Number, Email_id, Website, Address, Image) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)""")
+                    val = (row['Name'], row['Designation'], row['Mobile_Number'],row['Alternate_Number'], row['Email_id'], row['Website'], row['Address'], binaryData)
                     cursor = conn.cursor()
                     cursor.execute(sql, val)
                     conn.commit()
