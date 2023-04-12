34be590acc7d153caf3d3756e7df6895874ab1c6
Srinivasan G <46883734+SrinivasanGajendran@users.noreply.github.com>
2023-04-12 09:53:32 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -6,17 +6,15 @@ import pandas as pd
 import tempfile
 import mysql.connector
 import os
+import io
 
-#-------------------------------------------Page_Config------------------------------------
 
 st.set_page_config(page_title='TEXT_EXTRACTION_USING_OCR', page_icon=":tada", layout='wide')
 st.title('Extraction Of data Using Python And EasyOCR')
 
-#----------------------------------------Creating a select Menu-----------------------------
-
 SELECT = option_menu(
     menu_title=None,
-    options=['Home', 'Process', 'Database'],
+    options=['Home', 'Process','Database'],
     icons=['house', 'bar-chart'],
     default_index=2,
     orientation='horizontal',
@@ -28,8 +26,6 @@ SELECT = option_menu(
     }
 )
 
-#----------------------Home--------------------------------------
-
 if SELECT == 'Home':
     st.write('Extraction of data using Python and EasyOCR refers to the process of using the EasyOCR library in Python to extract text data from images, PDFs, or other types of documents. EasyOCR is a Python library that provides a simple way to perform Optical Character Recognition (OCR) on images or scanned documents, using deep learning models.'
              'With EasyOCR, you can extract text from images in various languages, such as English, Spanish, French, Chinese, and many more. The library is also able to handle text in various fonts and styles, including handwriting, making it a versatile tool for data extraction tasks.'
@@ -38,8 +34,6 @@ if SELECT == 'Home':
     image = Image.open('D:/python/Capstone_4/ocr.jpg')
     st.image(image, caption='EasyOCR')
 
-#------------------------Process--------------------------------
-    
 elif SELECT == 'Process':
     uploaded_file = st.file_uploader('Choose a image file')
     if uploaded_file is not None:
@@ -49,10 +43,7 @@ elif SELECT == 'Process':
         if st.button('Upload'):
             main.extraction(uploaded_filename)
             st.success('Created DB')
-        os.unlink(uploaded_filename) #-------------------------> To delete the temp file created
-        
-        
-#----------------------------Database---------------------------
+        os.unlink(uploaded_filename)
 
 elif SELECT == 'Database':
                 conn = mysql.connector.connect(
@@ -65,17 +56,21 @@ elif SELECT == 'Database':
                 df1 = pd.read_sql_query('SELECT * FROM Business_Card', conn)
                 column_names = df1.columns.tolist()
                 options = st.selectbox('Search :-',(column_names))
-                title = st.text_input(f'{options}')
+                df3 = pd.read_sql_query(f"""SELECT {options} FROM Business_Card """, conn)
+                title = st.selectbox(f'Search by {options}',df3)
                 df2 = pd.read_sql_query(f"""SELECT * FROM Business_Card WHERE {options} = '{title}' """, conn)
+                st.write(df2)
                 cursor.execute(f"SELECT Image FROM Business_Card WHERE {options} = '{title}' ")
                 result = cursor.fetchone()
                 image_data = result[0]
                 image = Image.open(io.BytesIO(image_data))
                 if st.button('View Card'):
-                    st.write(df2)
                     st.image(image, caption='Image')
                 if st.button('View DB'):
                     df = pd.read_sql_query('SELECT * FROM Business_Card', conn)
                     st.write(df)
                 conn.commit()
                 conn.close()
+
+
+
